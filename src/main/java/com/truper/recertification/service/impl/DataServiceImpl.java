9f6f126a5d382ccd5575cf799d75a8593ae77a56
Hugo Daniel Rodriguez Bautista <hdrodriguezb@truper.com>
2019-11-26 11:18:11 -0600

    Cambios para obtener banderas sobre si el jefe se encuentra recertificado o si ya inicio el proceso de recertificacion

--- src/main/java/com/truper/recertification/service/impl/DataServiceImpl.java
+++ src/main/java/com/truper/recertification/service/impl/DataServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,14 @@ import com.truper.recertification.dao.ReControlCambiosDAO;
 import com.truper.recertification.dao.ReDepartamentoDAO;
 import com.truper.recertification.dao.ReDetalleJefeDAO;
 import com.truper.recertification.dao.RePerfilSistemaDAO;
+import com.truper.recertification.dao.ReRecertificacionDAO;
 import com.truper.recertification.dao.ReSistemaDAO;
+import com.truper.recertification.model.PKRecertificacion;
 import com.truper.recertification.model.ReBitacoraCambiosEntity;
 import com.truper.recertification.model.ReControlCambiosEntity;
 import com.truper.recertification.model.ReDetalleJefeEntity;
 import com.truper.recertification.model.RePerfilSistemaEntity;
+import com.truper.recertification.model.ReRecertificacionEntity;
 import com.truper.recertification.model.ReSistemaEntity;
 import com.truper.recertification.service.DataService;
 import com.truper.recertification.vo.answer.SystemsVO;
@@ -44,6 +48,9 @@ public class DataServiceImpl implements DataService{
 	
 	@Autowired
 	private ReDepartamentoDAO daoDepartamento;
+	
+	@Autowired
+	private ReRecertificacionDAO daoRecertificacion;
 	
 	@Override
  	public List<SystemsVO> getSystems() {
@@ -127,17 +134,53 @@ public class DataServiceImpl implements DataService{
 	}
 
 	@Override
-	public Map<String, Object> findByAllBoss() {
+	public Map<String, Object> findByAllBoss(String periodo) {
 		Map<String, Object> bossMap = new HashMap<>();
 		
 		List<ReDetalleJefeEntity> lstDetail = daoJefe.findAll();
 		List<BossDetailVO> lstBoss = new ArrayList<>();
 		
 		for(int i = 0; i < lstDetail.size(); i++) {
-			lstBoss.add(this.findByBoss(lstDetail.get(i).getId()));
+			lstBoss.add(this.findBossForRecertification(lstDetail.get(i).getId(), periodo));
 		}
 		bossMap.put("jefes",lstBoss);
 		
 		return bossMap;
+	}
+
+	@Override
+	public BossDetailVO findBossForRecertification(String idJefe, String periodo) {
+		
+		BossDetailVO bossVO = new BossDetailVO();
+		
+		ReDetalleJefeEntity detailBoss = daoJefe.findById(idJefe).get();
+		bossVO.setIdJefe(idJefe);
+		bossVO.setNombre(detailBoss.getNombre());
+		bossVO.setCorreo(detailBoss.getCorreo());
+		bossVO.setCorreoCC(detailBoss.getCorreoCC());
+		bossVO.setDepartamento(daoDepartamento.findById(
+				detailBoss.getIdDepartamento()).get().getDepartamento());
+		
+		ReRecertificacionEntity jefeRecertificado = null;
+		
+		try {
+			jefeRecertificado = daoRecertificacion.findById(PKRecertificacion.builder()
+					.idJefe(idJefe).periodo(periodo).build()).get();
+			if(jefeRecertificado != null) {
+				bossVO.setInRecertificacion(true);
+				if(jefeRecertificado.isEstatus()) {
+					bossVO.setRecertificado(true);
+				}else {
+					bossVO.setRecertificado(false);
+				}
+			}else {
+				bossVO.setInRecertificacion(false);
+			}
+		}catch(NoSuchElementException nsee ) {
+			bossVO.setInRecertificacion(false);
+			bossVO.setRecertificado(false);
+		}
+		
+		return bossVO;
 	}
 }
--- src/main/java/com/truper/recertification/vo/answer/BossDetailVO.java
+++ src/main/java/com/truper/recertification/vo/answer/BossDetailVO.java
@@ -25,4 +25,8 @@ public class BossDetailVO implements Serializable{
 
 	private String departamento;
 	
+	private boolean isInRecertificacion;
+	
+	private boolean isRecertificado;
+	
 }
